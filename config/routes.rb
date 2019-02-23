96b51a729cc75bae8f530e12c36939767832a5fb
Madison Stankevich <madison.stankevich@flatironschool.com>
2019-02-23 10:20:18 -0800

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -53,4 +53,6 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+  resources :posts, only: :show
+  #get 'posts/:id', to: 'posts#show' (above line replaces this code)
 end
